05ccbf9a9655d3c13dba199866df948f7978afa3
maruthiganesh <67068719+maruthiganesh@users.noreply.github.com>
2023-10-18 16:09:57 +0530

    card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from "@angular/common/http"
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
-import { MovieListComponent } from './movie-card/movie-list/movie-list.component';
+
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MovieService } from './services/movie.service';
+import { MovieListComponent } from './movie-list/movie-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieCardComponent,
-    MovieListComponent,
-    NavBarComponent
+    NavBarComponent,
+    MovieListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [MovieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,17 +1,17 @@
-import { Component } from "@angular/core";
 
+
+import { Component , Input} from "@angular/core";
 @Component(
 
     {
         selector:'app-movie-card',
         //template:`<h3>movie ticket booking<h3>`
         templateUrl:'movie-card.component.html',
-        styleUrls: ['movie-card.component.css']
-
+        styleUrls: ['movie-card.component.css'],
 
     }
 )
 
 export class MovieCardComponent{
-
+    @Input () movie_details: any 
 }
