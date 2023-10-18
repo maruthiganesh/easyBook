41aa83df5732c830d5ce50782948923a3ce73bfd
maruthiganesh <67068719+maruthiganesh@users.noreply.github.com>
2023-10-18 23:27:02 +0530

    changes in nav and card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,34 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MovieService } from './services/movie.service';
 import { MovieListComponent } from './movie-list/movie-list.component';
 
+import { Routes, RouterModule } from '@angular/router';
+import { MyBookingsComponent } from './my-bookings/my-bookings.component';
+import { UserLoginComponent } from './User/user-login/user-login.component';
+import { UserRegisterComponent } from './User/user-register/user-register.component';
+
+const appRoutes:Routes=[
+  {path: '', component: MovieListComponent},
+  {path:'my-bookings',component:MyBookingsComponent},
+  {path:'login',component:UserLoginComponent},
+  {path:'register',component:UserRegisterComponent}
+]
+
 @NgModule({
   declarations: [
     AppComponent,
     MovieCardComponent,
     NavBarComponent,
-    MovieListComponent
+    MovieListComponent,
+    MyBookingsComponent,
+    UserLoginComponent,
+    UserRegisterComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [MovieService],
   bootstrap: [AppComponent]
