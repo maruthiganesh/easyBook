67f2f97faec6787d8d59463a44557a7af5f2a55e
maruthiganesh <67068719+maruthiganesh@users.noreply.github.com>
2023-11-06 22:35:34 +0530

    correction in filter

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MovieService } from '../services/movie.service';
+import { take } from 'rxjs';
 
 let mapper = new Map();
 mapper.set("id_blore", "Bangalore");
@@ -18,20 +19,27 @@ export class NavBarComponent {
   onChange(event: any) {
     let element = event.target;
     let eid = element.id;
-
+   
     if (element.checked) {
-      this.movieService.checkedArray$.subscribe(checkedArray => {
+      this.movieService.checkedArray$.pipe(take(1)).subscribe(checkedArray => {
         if (!checkedArray.includes(mapper.get(eid))) {
           const updatedArray = [...checkedArray, mapper.get(eid)];
           this.movieService.updateCheckedArray(updatedArray);
+
         }
       });
-    } else {
-      this.movieService.checkedArray$.subscribe(checkedArray => {
+    } else  {
+      this.movieService.checkedArray$.pipe(take(1)).subscribe(checkedArray => {
+        console.log("==checked array in else===")
+        
         const index = checkedArray.indexOf(mapper.get(eid));
-        if (index !== -1) {
+        
+        if (index !== -1) {         
+
           const updatedArray = checkedArray.slice();
+         
           updatedArray.splice(index, 1);
+          
           this.movieService.updateCheckedArray(updatedArray);
         }
       });
